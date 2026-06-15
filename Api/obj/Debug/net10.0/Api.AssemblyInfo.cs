760aa471c45d785365482997b36b9240331119f9
ms5788 <ms5788@drexel.edu>
2026-06-15 01:41:57 -0400

    Add AppDbContext for EF Core integration and update assembly version

--- Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
+++ Api/obj/Debug/net10.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ab55828b94e9df762da7a3c99a6905b253db4e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb1b69893b91cb4770173c212057429b56829a43")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
